3ddb729646a280dcab4ace5ad0550af11cc0e20a
romainmltr <romain.malaterre@joyjet.com>
2023-07-05 11:46:45 +0200

    add the files .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-undef": "error",
+        'semi': 'error',
+        'no-console': 'off',
+        'no-unused-vars': 'off',
+        'no-undef-init': 'off',
+
+
+
+    }
+}
